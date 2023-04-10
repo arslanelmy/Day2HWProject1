5ad33dbcdbb24cd3922733f4faa51951c14250e6
Phoenix <Phoenix@10.0.0.221>
2023-04-10 17:14:53 -0400

    stashed commit

--- src/test/demo1.java
+++ src/test/demo1.java
@@ -2,4 +2,10 @@ package test;
 
 public class demo1 {
 
+	public static void main(String[] args) {
+		System.out.println("Hello World..");
+	}
+	public void add() {
+		System.out.println(1+2);
+	}
 }
